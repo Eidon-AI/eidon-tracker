fa9a5c3a15d8af9eb43b5f328bdd615f57c2d94f
theSamPadilla <sam@eidon.ai>
2026-02-15 21:40:59 -0600

    Fixing version bump

--- firmware/src/Version.h
+++ firmware/src/Version.h
@@ -2,8 +2,8 @@
 #define FIRMWARE_VERSION_H
 
 #define FIRMWARE_VERSION_MAJOR 2
-#define FIRMWARE_VERSION_MINOR 0
-#define FIRMWARE_VERSION_PATCH 1
+#define FIRMWARE_VERSION_MINOR 1
+#define FIRMWARE_VERSION_PATCH 0
 
 // String representation for BLE Device Information Service
 #define FIRMWARE_VERSION_STRING "2.1.0"
